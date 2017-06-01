d001685cc9e32dbb5d148b07a37c537a6f8f72d3
Denis Vanoni <dex.vanoni@gmail.com>
2017-06-01 15:20:41 -0400

    Em desenvolvimento (ficha de OS)

--- resources/views/ficha.blade.php
+++ resources/views/ficha.blade.php
@@ -1,17 +1,20 @@
 @extends('template')
 
-@section('topo')
+@section('title')
+  O.S - Solicitação de Diárias
+@endsection
 
+@section('topo')
   <div class="row">
     <div class="col-md-4">
       <img src="/bst/brasao.png" class="img-responsive" alt="" height="70em" width="70em" />
     </div>
     <div class="col-md-4">
       <center>
-      <h4>COMANDO DA AERONÁUTICA</h4>
-      <p>GRUPAMENTO DE APOIO DE CAMPO GRANDE</p>
-      <p>Solicitação nº SC {{ $os }}</p>
-    </center>
+        <h4>COMANDO DA AERONÁUTICA</h4>
+        <p>GRUPAMENTO DE APOIO DE CAMPO GRANDE</p>
+        <p>Solicitação nº SC {{ $os }}</p>
+      </center>
     </div>
     <div class="col-md-4">
       <center>
@@ -19,7 +22,25 @@
         <p>Nº PCDP: {{ $pcdp }}</p>
       </center>
     </div>
-
   </div>
+@endsection
 
+@section('sec1')
+  <div class="row">
+    <h5><b>1 - Determinação:</b></h5>
+  </div>
+  <div class="row">
+    <h6>Determino ao militar/servidor civil abaixo que realize o serviço especificado, fora da sede desta OM, nas condições seguintes:</h6>
+  </div>
+  <div class="row">
+    <div class="col-md-7">
+      <input type="text" id="pnome" name="pnome" class="form-control input-sm" placeholder="POSTO/GRAD/NOME:">
+    </div>
+  <div class="col-md-2">
+    <input type="text" id="saram" name="saram" class="form-control input-sm" placeholder="SARAM:">
+  </div>
+  <div class="col-md-3">
+    <input type="text" id="cpf" name="cpf" class="form-control input-sm" placeholder="CPF:">
+  </div>
+</div>
 @endsection
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -11,41 +11,43 @@
 <body>
   <!--Nav Bar-->
   <nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <i class="navbar-brand">Solicitação de Diárias</i>
-    </div>
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <i class="navbar-brand">Solicitação de Diárias</i>
+      </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <form class="navbar-form navbar-left">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Procurar OS">
-        </div>
-        <button type="submit" class="btn btn-default">Procurar</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="http://10.152.16.203/gapcg">GAP-CG</a></li>
-        <li><a href="http://10.152.16.203/ala5">ALA5</a></li>
-      </ul>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <form class="navbar-form navbar-left">
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="Procurar OS">
+          </div>
+          <button type="submit" class="btn btn-default">Procurar</button>
+        </form>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="http://10.152.16.203/gapcg">GAP-CG</a></li>
+          <li><a href="http://10.152.16.203/ala5">ALA5</a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
   <div class="container">
     @yield('topo')
     <hr>
-    @yield('sec1')
+    <form class="" action="index.html" method="post">
+      @yield('sec1')
+      
+      @yield('sec2')
 
-    @yield('sec2')
-    
-    @yield('sec3')
+      @yield('sec3')
+    </form>
   </div>
 </body>
 <script src="/bst/js/jquery.min.js"></script>
