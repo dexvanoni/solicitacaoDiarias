873ba05719cd73d4dbc8bc4a267f9629fa10addd
Denis Vanoni <dex.vanoni@gmail.com>
2017-06-06 16:22:55 -0400

    Terminado o form de preenchimento da ficha - Seguir para banco de dados

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -57,7 +57,6 @@
       @yield('sec2')
       <hr>
       @yield('sec3')
-      <hr>
     </form>
   </div>
   <script type="text/javascript">
@@ -72,7 +71,7 @@
     });
   });
   </script>
-  <!-- CALCULA OS IPUTS DE VALOR E QUANTIDADE DA TABELA DE CÔMPUTO DE DIÁRIAS-->
+  <!-- CALCULA OS INPUTS DE VALOR E QUANTIDADE DA TABELA DE CÔMPUTO DE DIÁRIAS-->
   <!-- PRIMEIRA LINHA -->
   <script type="text/javascript">
   $(document).ready( function() {
@@ -126,6 +125,68 @@
   });
   </script>
   <!--TERMINA CÁLCULO DA TABELA-->
+
+  <!-- MULTIPLICAÇÃO DE DESCONTOS -->
+  <!-- PRIMEIRA LINHA-->
+  <script type="text/javascript">
+  $(document).ready( function() {
+    $('#desc_a, #qt_dias_a').blur(function(){
+      var valor 		= $('#desc_a').val();
+      var qt = $('#qt_dias_a').val();
+      if(valor == "") valor = 0;
+      if(qt == "") qt = 0;
+      var resultado 	= parseFloat(valor) * parseFloat(qt);
+      $('#resultado_dias_a').val(resultado);
+    })
+  });
+  </script>
+  <!-- SEGUNDA LINHA-->
+  <script type="text/javascript">
+  $(document).ready( function() {
+    $('#desc_b, #qt_dias_b').blur(function(){
+      var valor 		= $('#desc_b').val();
+      var qt = $('#qt_dias_b').val();
+      if(valor == "") valor = 0;
+      if(qt == "") qt = 0;
+      var resultado 	= parseFloat(valor) * parseFloat(qt);
+      $('#resultado_dias_b').val(resultado);
+    })
+  });
+  </script>
+
+  <!--SOMA E DESCONTO DE TOTAIS-->
+  <script type="text/javascript">
+  $(document).ready( function() {
+    $('#resultado1, #resultado2, #resultado3, #resultado4, #val_ac, #resultado_dias_a, #resultado_dias_b').blur(function(){
+      var v1 		= $('#resultado1').val();
+      var v2 		= $('#resultado2').val();
+      var v3 		= $('#resultado3').val();
+      var v4 		= $('#resultado4').val();
+      var v5 		= $('#val_ac').val();
+
+      var v6 		= $('#resultado_dias_a').val();
+      var v7 		= $('#resultado_dias_b').val();
+
+      if(v1 == "") v1 = 0;
+      if(v2 == "") v2 = 0;
+      if(v3 == "") v3 = 0;
+      if(v4 == "") v4 = 0;
+      if(v5 == "") v5 = 0;
+      if(v6 == "") v6 = 0;
+      if(v7 == "") v7 = 0;
+
+      var resultado1 	= parseFloat(v1) + parseFloat(v2) + parseFloat(v3) + parseFloat(v4) + parseFloat(v5);
+      $('#resultado_soma').val(resultado1);
+
+      var resultado2 	= parseFloat(v6) + parseFloat(v7);
+      $('#resultado_descontos').val(resultado2);
+
+      var resultado 	= parseFloat(resultado1) - parseFloat(resultado2);
+      $('#resultado_total').val(resultado);
+    })
+  });
+  </script>
+
   <script src="/bst/js/bootstrap.min.js"></script>
 </body>
 </html>
